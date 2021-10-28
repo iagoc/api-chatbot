4952345504b194cd5cfed9f2d63d1d755fb64733
Iago <iagosilva.ti@gmail.com>
2021-10-27 23:52:06 -0300

    alteração na api

--- src/services/github/index.js
+++ src/services/github/index.js
@@ -1,20 +1,25 @@
 const axios = require("axios");
 
-const url = 'https://api.github.com/search/repositories?q=org:takenet+language:c%23'
-// const options = {
-//     method: 'GET',
-//     headers: {
-//         "Content-Type": "application/json",
-//     }
-// }
+const url = `https://api.github.com/users/`
 
-exports.request = async (method = 'get') => {
-  const response = await axios.get(url)
+exports.request = async () => {
+  const paramRepo = 'takenet/'
+  const paramOrder ='repos?sort=created&direction=asc&per_page=6'
 
-  response.data.items.forEach((item, key) => {
-    console.log(key)
-    console.log(item.description)
-  })
+  const response = await axios.get(url + paramRepo + paramOrder)
 
-  return response.data
+  const resultFilterRepo = response.data.filter(repo => repo.language === 'C#');
+
+  const resultFinal = new Array();
+  resultFilterRepo.forEach((repo) => {
+    resultFinal.push({
+      img: repo.owner.avatar_url,
+      name: repo.name,
+      desc: repo.description,
+      lang: repo.language,
+      date_create: repo.created_at
+    });
+  });
+
+  return resultFinal;
 }
